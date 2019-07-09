7625c7ca21c85acdedb24b18fd01d71fa9b734ea
operaxxx <a3303603@live.com>
2019-07-09 23:58:25 +0800

    Add slot
    使用插槽，并通过作用域插槽控制样式

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,34 +1,29 @@
 <template>
-		<li>
-			<Checkbox v-model="isChecked"></Checkbox>
-			{{todo.content}}
-			<a href="#" style="margin-left:20px" @click="$emit('remove')">
-				<Icon type="md-close" />
-			</a>
-		</li>
+  <li>
+    <Checkbox v-model="isChecked"></Checkbox>
+    <slot name="todoContent" v-bind="{isChecked}"></slot>
+    <a href="#" style="margin-left:20px" @click="$emit('remove')">
+      <Icon type="md-close" />
+    </a>
+  </li>
 </template>
 
 <script>
 export default {
-	props : {
-		todo : {
-		},
-		index : {
-
-		}
-	},
-	data() {
-		return {
-			isChecked : false
-		}
-	},
+  props: {
+    todo: {},
+    index: {}
+  },
+  data() {
+    return {
+      isChecked: false
+    };
+  }
 };
 </script>
 
 <style scoped>
-	li {
-		padding-bottom: 4px;
-		color:dimgray;
-		font-size: 18px;
-	}
+li {
+  padding-bottom: 4px;
+}
 </style>
--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,32 +1,33 @@
 <template>
   <div>
     <Card style="width:600px;margin:auto">
-      <p slot="title" style="text-align:left" ><Icon type="ios-paper" /> 待办事项</p>
-      <a href="#" slot="extra"
-				@click.prevent = 'openAddModal'
-				>
+      <p slot="title" style="text-align:left">
+        <Icon type="ios-paper" />待办事项
+      </p>
+      <a href="#" slot="extra" @click.prevent="openAddModal">
         <Icon type="md-add" />
       </a>
-			<ul class="todo_ul">
-				<todo-item
-					v-for="(todo,index) in todoList"
-					:key="todo.id"
-					:index="index"
-					@remove="remove(index)"
-					:todo="todo"
-				>
-			</todo-item>
-			</ul>
+      <ul class="todo_ul">
+        <template></template>
+        <todoItem
+          v-for="(todo,index) in todoList"
+          :key="todo.id"
+          :index="index"
+          @remove="remove(index)"
+          :todo="todo"
+        >
+          <template v-slot:todoContent="contentProps">
+            <span
+              :style="{fontSize : '18px', textDecoration : contentProps.isChecked ? 'line-through' : ''}"
+            >{{todo.content}}</span>
+          </template>
+        </todoItem>
+      </ul>
     </Card>
 
-		<Modal
-			v-model="addTodoModal"
-			:mask-closable="false"
-			@on-ok="addTodo"
-			@on-cancel="addCancle"
-			>
-			<Input v-model="newTodoText" placeholder="请输入待办事项..." style="width:300px" />
-		</Modal>
+    <Modal v-model="addTodoModal" :mask-closable="false" @on-ok="addTodo" @on-cancel="addCancle">
+      <Input v-model="newTodoText" placeholder="请输入待办事项..." style="width:300px" />
+    </Modal>
   </div>
 </template>
 
@@ -46,39 +47,37 @@ export default {
         { id: 2, content: "理发" },
         { id: 3, content: "跑步" }
       ],
-			todoNextId : 4,
-			addTodoModal : false,
-			newTodoText : '',
+      todoNextId: 4,
+      addTodoModal: false,
+      newTodoText: ""
     };
   },
-	methods: {
-		addTodo : function () {
+  methods: {
+    addTodo: function() {
+      if (!this.newTodoText) {
+        this.$Message.warning("请输入待办事项");
+        return;
+      }
 
-			if(!this.newTodoText) {
-				this.$Message.warning("请输入待办事项") ;
-				return ;
-			}
+      this.todoList.push({
+        id: this.todoNextId++,
+        content: this.newTodoText
+      });
 
-			this.todoList.push({
-				id : this.todoNextId++,
-				content : this.newTodoText
-			})
-
-			this.newTodoText = '' 
-			this.$Message.info("添加成功")
-		},
-		remove : function (index) {
-			var index = event.target.index ;
-			this.todoList.splice(index, 1) ;
-			this.$Message.info("移除")
-		},
-		openAddModal : function () {
-			this.addTodoModal = true 
-		},
-		addCancle : function () {
-			this.newTodoText = '' ;
-		}
-	},
+      this.newTodoText = "";
+      this.$Message.info("添加成功");
+    },
+    remove: function(index) {
+      this.todoList.splice(index, 1);
+      this.$Message.info("移除");
+    },
+    openAddModal: function() {
+      this.addTodoModal = true;
+    },
+    addCancle: function() {
+      this.newTodoText = "";
+    }
+  }
 };
 </script>
 
@@ -89,8 +88,8 @@ export default {
 }
 
 .todo_ul {
-	text-align: left ;
-	padding-left: 5px ;
-	list-style-type: none ;
+  text-align: left;
+  padding-left: 5px;
+  list-style-type: none;
 }
 </style>
